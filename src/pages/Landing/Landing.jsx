9af43db2f401bd1d91ac76b8e14be16fedecebe1
Anthony Madia <aamadia24@gmail.com>
2022-03-24 20:58:27 -0400

    Show cloudinary links

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,18 +1,29 @@
 import styles from './Landing.module.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, Navigate } from 'react-router-dom'
+import { getNewsFeed } from '../../services/postService'
 
-const Landing = ({ user }) => {
-
+const Landing = ({ user, getPosts }) => {
+  const [newsFeed, setNewsFeed] = useState([])
   const navigate = useNavigate()
-
+  
   useEffect(() => {
-    if (!user) navigate('login')
+    (async () => { let ben = await getNewsFeed(); setNewsFeed(ben)})()
+    // let returnedNewsFeed = getNewsFeed()
+    // console.log(returnedNewsFeed)
+    // if (!user) Navigate('login')
+    // setNewsFeed(returnedNewsFeed)
   }, [])
 
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+      {console.log(newsFeed)}
+      <ul>
+        {newsFeed?.map(post => (
+          <li>{post?.images}</li>
+        ))}
+      </ul>
     </main>
   )
 }
