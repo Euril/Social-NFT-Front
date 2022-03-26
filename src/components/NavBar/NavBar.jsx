d3d64030e3658ce76c51b93441d7033ff3a7aace
Anthony Madia <aamadia24@gmail.com>
2022-03-26 11:24:24 -0400

    Save navbar changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,18 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 const NavBar = ({ user, handleLogout, profile }) => {
   //console.log('profile in nav bar', profile)
-  const [params, setParams] = useState(useParams())
-
-  useEffect(()=>{
-
-  }, [params])
+  const location = useLocation()
+  const navigate = useNavigate()
+  console.log(location)
+  // useEffect(()=>{
+  // }, [])
+  
+  const pageChange = () => {
+    
+    navigate(location.pathname)
+  }
   
   return (
     <>
@@ -18,9 +23,9 @@ const NavBar = ({ user, handleLogout, profile }) => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/messages">Messages</Link></li>
             <li><Link to="/addpost">Add Post</Link></li>
-            <li><Link to="/explore">Explore</Link></li>
+            <li><Link to="/explore" onClick={pageChange}>Explore</Link></li>
             <li><Link to="/notifications">Notifications</Link></li>
-            <li><Link to={`${profile.email}`}>Profile</Link></li>
+            <li><Link to={`${profile.email}`} onClick={pageChange}>Profile</Link></li>
             <li><Link to="" onClick={handleLogout}>Log Out</Link></li>
           </ul>
         </nav>
