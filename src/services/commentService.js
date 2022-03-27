05709d8c975d39a8bcefe715387e9f3db018c3e3
Anthony Madia <aamadia24@gmail.com>
2022-03-26 21:35:53 -0400

    AAU, i can add a comment and see it rendered

---
+++ src/services/commentService.js
@@ -0,0 +1,21 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/comments`
+//const NEWSFEED_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/posts/newsfeed`
+//const EXPLORE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/posts/explore`
+
+function createComment(comment) {
+    return fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+      },
+      body: JSON.stringify(comment)
+      //body: comment
+    })
+    .then(res => res.json())
+  }
+
+export {
+    createComment
+}
