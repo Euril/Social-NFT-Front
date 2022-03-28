f2b9cdc6a61fe3079dab821825f265ab5ccc9848
EugeneR <eugenerillo@gmail.com>
2022-03-28 01:40:07 -0400

    style to otherprofile ourprofilepage add add profilemodulecss

--- src/pages/Profile/LoadedProfilePage.jsx
+++ src/pages/Profile/LoadedProfilePage.jsx
@@ -1,10 +1,11 @@
 import OurProfilePage from "./OurProfilePage"
 import OthersProfilePage from "./OthersProfilePage"
+import styles from './Profile.module.css'
 
 const LoadedProfilePage = ({profiles, loggedInUser, profToRender, ourProfile, handleFollow, handleUnfollow, profile}) => {
 
     return (
-        <div>
+        <div className={styles.container}>
 
             {
                 
